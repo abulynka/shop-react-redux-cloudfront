3497def355885a024d877d91388dfa09285853e3
Aliaksandr Bulynka <abulynka@gmail.com>
2021-10-13 15:10:04 +0300

    feat: add authorization

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -30,23 +30,32 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
   };
 
   const uploadFile = async (e: any) => {
-      // Get the presigned URL
-      const response = await axios({
-        method: 'GET',
-        url,
-        params: {
-          name: encodeURIComponent(file.name)
-        }
-      })
-      console.log('File to upload: ', file.name)
-      console.log('Uploading to: ', response.data)
-      const result = await fetch(response.data, {
-        method: 'PUT',
-        body: file
-      })
-      console.log('Result: ', result)
-      setFile('');
+    const authorizationToken = localStorage.getItem('authorization_token');
+    let headers = {};
+    if (authorizationToken) {
+      headers = {
+        Authorization: "Basic " + authorizationToken
+      };
     }
+
+    // Get the presigned URL
+    const response = await axios({
+      method: 'GET',
+      url,
+      params: {
+        name: encodeURIComponent(file.name)
+      },
+      headers: headers
+    })
+    console.log('File to upload: ', file.name)
+    console.log('Uploading to: ', response.data)
+    const result = await fetch(response.data, {
+      method: 'PUT',
+      body: file
+    })
+    console.log('Result: ', result)
+    setFile('');
+  }
   ;
 
   return (
