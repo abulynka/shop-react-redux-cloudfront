813ca479b33dbe9873a4b8fb0cb826c77ca2e5b6
Aliaksandr Bulynka <abulynka@gmail.com>
2021-10-25 00:45:27 +0300

    feat: add path to ebt

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://rawukoru6g.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://5g9yf9dqqh.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://rawukoru6g.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://rawukoru6g.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://abulynka-cart-api-test.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
